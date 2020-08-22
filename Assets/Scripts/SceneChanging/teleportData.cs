6a7ca696061b21a7f116abf01d388060b47a7410
dbooher <dbooher46@gmail.com>
2020-08-22 10:38:07 -0500

    Removed slope code added in scene changing
    I started work on blocking out the map and transitioning between scenes, I also got rid of all my slope code.

---
+++ Assets/Scripts/SceneChanging/teleportData.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleportData : MonoBehaviour
+{
+    public string sceneFrom = "" ;
+    public int sceneToId = 0 ;
+    public bool loadingStarted = false;
+    public bool loadingFinished = false;
+    AsyncOperation loadingOperation;
+    public GameObject playerPrefab;
+    // Start is called before the first frame update
+
+    private void Start()
+    { 
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    private void Update()
+    {
+        if(loadingStarted)
+        {
+            if(loadingOperation.isDone)
+            {
+                loadingFinished = true;
+                teleportTarget[] targets = GameObject.FindObjectsOfType<teleportTarget>();
+                foreach(teleportTarget t in targets)
+                {
+                    if(t.sceneFrom == sceneFrom)
+                    {
+                        if(GameObject.Find("Player"))
+                        {
+                            Destroy(GameObject.Find("Player"));
+                        }
+                        GameObject p = Instantiate(playerPrefab, t.transform.position, Quaternion.identity);
+                        p.name = "Player";
+                    }
+                }
+                Finish();
+            }
+        }
+    }
+
+    void Finish()
+    {
+        Destroy(gameObject);
+    }
+
+    public void startLoad()
+    {
+        loadingOperation = SceneManager.LoadSceneAsync(sceneToId);
+        loadingStarted = true;
+    }
+}
---
+++ Assets/Scripts/SceneChanging/teleportZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleportZone : MonoBehaviour
+{
+    public int targetScene;
+    public string sceneFrom;
+    public GameObject teleportDataPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void teleport()
+    {
+        teleportData td = Instantiate(teleportDataPrefab).GetComponent<teleportData>();
+        td.sceneToId = targetScene;
+        td.sceneFrom = sceneFrom;
+        td.startLoad();
+    }
+}
