11ac38712d3c701bad6d059b5b74ade74e33a9e8
dbooher <dbooher46@gmail.com>
2020-08-21 19:27:39 -0500

    More Work On Horizontal Collisions
    I ended up forgoing the script method and am just letting the rigidbody do the work, I will need to update this more however as the current implimentation allows the player into the wall with eneough effort then the whole thing breaks.

--- Assets/Scripts/Player/playerCollisionHandler.cs
+++ Assets/Scripts/Player/playerCollisionHandler.cs
@@ -9,19 +9,21 @@ public class playerCollisionHandler : MonoBehaviour
     public playerMovement movement;
     public lockToGround gLock;
     public LayerMask solidMask;
+    public Vector2 gizmoPoint;
+    public bool drawGizmo = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         movement = GetComponent<playerMovement>();
-        gLock = movement.lockGround;
+        gLock = GetComponent<lockToGround>();
     }
 
     // Update is called once per frame
-    float horizontalColCheck(float xOffset)
+    public float horizontalColCheck(float xOffset, float xOffsetIncrease)
     {
 
-        float newXOffset = xOffset;
+        float newXOffset = xOffset + xOffsetIncrease;
         //if the player is facing right keep xMod posative, otherwise its negative
         int xMod = (gLock.facingRight ? 1 : -1);
         Vector2 castDirection = new Vector2(xMod, 0);
@@ -39,6 +41,14 @@ public class playerCollisionHandler : MonoBehaviour
         //determine which is the closest
         float min = Mathf.Min(distanceTop, distanceMiddle, distancebottom);
 
+        if(top || middle || bottom)
+        {
+            drawGizmo = true;
+        }
+        else
+        {
+            drawGizmo = false;
+        }
         //set the correct raycast data to use
         RaycastHit2D closest=top;
         if(min==distanceTop)
@@ -65,8 +75,26 @@ public class playerCollisionHandler : MonoBehaviour
         }
 
         Vector2 closestPoint = closest.point;
+        gizmoPoint = closestPoint;
+
+        if(!gLock.facingRight)
+        {
+             if(gLock.basePosition.x - xOffset - xOffsetIncrease - (gLock.box.size.x / 2) < closestPoint.x)
+            {
+                newXOffset = closestPoint.x + ( gLock.box.size.x / 2);
+            }
+        }
 
 
         return newXOffset;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(drawGizmo)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(gizmoPoint, .25f);
+        }
     }
 }
