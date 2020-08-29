2da2d34d2807693fa6766d90135374f4f688cd0d
dbooher <dbooher46@gmail.com>
2020-08-29 10:54:27 -0500

    A Lot of Art Objects
    Added in a ton of small objects (see notes from 8/29) as well as 2 large objects (clock and window) added in lighting and shadows to the 3 finished rooms (just the basics still have to add some objects)

--- Assets/Scripts/Player/cameraFollow.cs
+++ Assets/Scripts/Player/cameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class cameraFollow : MonoBehaviour
 {
     public Rigidbody2D objToFollow;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,6 @@ public class cameraFollow : MonoBehaviour
         {
             objToFollow = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         }
-        transform.position = new Vector3(objToFollow.position.x,objToFollow.position.y,-10);
+        transform.position = new Vector3(objToFollow.position.x,objToFollow.position.y,-10) + offset;
     }
 }
---
+++ Assets/Scripts/World/Objects/swing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swing : MonoBehaviour
+{
+    public Vector2 minMaxAngle;
+    public Vector2 minMaxSpeedMult;
+    public float timePerSwing;
+    public float currentSpeed;
+    public bool positive = true;
+    public float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer<timePerSwing)
+        {
+            timer += Time.deltaTime * currentSpeed;
+        }
+        else
+        {
+            timer = 0;
+            currentSpeed = Random.Range(minMaxSpeedMult.x, minMaxSpeedMult.y);
+            if(positive)
+            {
+                positive = false;
+            }
+            else
+            {
+                positive = true;
+            }
+        }
+        //do the swing
+        if(positive)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Mathf.Lerp(minMaxAngle.y, minMaxAngle.x, timer / timePerSwing));
+        }
+        else
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, Mathf.Lerp(minMaxAngle.x, minMaxAngle.y, timer / timePerSwing));
+        }
+    }
+}
